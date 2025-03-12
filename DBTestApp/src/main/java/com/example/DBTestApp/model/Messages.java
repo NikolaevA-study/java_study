99eea683a1da4aa0ee684ee6810312329e838f4b
NikolaevA-study <74500846+NikolaevA-study@users.noreply.github.com>
2025-03-12 09:09:46 +0300

    change GeneratedValue

--- DBTestApp/src/main/java/com/example/DBTestApp/model/Messages.java
+++ DBTestApp/src/main/java/com/example/DBTestApp/model/Messages.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 
 
 @Entity // указывает, что данный бин (класс) является сущностью
+//@SequenceGenerator(name = "messages_seq", sequenceName = "messages_seq")
 @Table(name = "messages") //указывает на имя таблицы, которая будет отображаться в этой сущности.
+@SequenceGenerator(name = "messages_seq", sequenceName = "messages_seq",allocationSize = 1)
 public class Messages {
 
     @Id
     @Column(name = "id") //на имя колонки, которая отображается в свойство сущности
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "messages_seq", strategy = GenerationType.SEQUENCE)
     //указывает, что данное свойство будет создаваться согласно указанной стратегии
     private Long id;
 
